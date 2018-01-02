acf76eecefddbf428a4f19103b1aeb36709e9af4
Dalon-ubuntu <dalonlobo@gmail.com>
2018-01-02 18:36:46 +0530

    youtube_video_downloader.py

--- ASR/youtube_video_downloader.py
+++ ASR/youtube_video_downloader.py
@@ -389,11 +389,10 @@ if __name__ == '__main__':
 	# V for video IDs, 'vi' for video id and information 
 	#dumpPlaylistorChannelInfo(sys.argv[1], 'vi')   #'v' for dumping only video ids 
 #	main(sys.argv[1])	
-    
-    df = pd.read_table('videos_for_benchmark.txt', header=None)
-    for index, playlistid in df.iterrows():
-        print("Reading this playlist id: ", playlistid[0])
-        main(playlistid[0])
+	df = pd.read_table('videos_for_benchmark.txt', header=None)
+	for index, playlistid in df.iterrows():
+		print("Reading this playlist id: ", playlistid[0])
+		main(playlistid[0])
 	#playlistid = 'tcsdigital'
 	#videoids = ['mM98zGIkr8Q','gQahEqw99fo','tfYx5ec_tTg','16BfEjrNzr4','yDOy5UewRZM','HsLSP8F4DqU','3PjwojBtLm8','ntn0O9BCNQI','BpgnnS7mKKU','r43LhSUUGTQ']
 
