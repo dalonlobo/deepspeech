3a7d090ef2dced2958e78105c12a7b86d8d86492
Dalon-ubuntu <dalonlobo@gmail.com>
2018-01-02 18:06:05 +0530

    text

---
+++ ASR/text_wer_entire_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  2 18:01:10 2018
+
+@author: dalonlobo
+"""
+
+import text
+
+if __name__ == "__main__":
+    with open("") as f1, open("") as f2:
+        text.wer(f1.read(), f2.read())
